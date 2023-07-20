454450e365fcbcf844851186d1d045cf62e6bd47
SantiagoTiberi <santiagomtiberi@gmail.com>
2023-07-20 17:02:51 -0300

    Paquetes

---
+++ Fundamentos/Paquetes/src/ar/com/global/Utileria.java
@@ -0,0 +1,12 @@
+
+package ar.com.global;
+
+/**
+ *
+ * @author santibox
+ */
+public class Utileria {
+    public static void imprimir(String s){
+        System.out.println("s =" + s);
+    }
+}
---
+++ Fundamentos/Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,15 @@
+
+package test;
+//import ar.com.global.Utileria;
+//import ar.com.global.*; //asi se importarian todas las clases del paquete
+//import static ar.com.global.Utileria.imprimir; //asi se importa solo el metodo imprimir
+public class TestUtileria {
+    public static void main(String[] args) {
+    //    Utileria.imprimir("hola");
+    //imprimir("adios");//asi se usa solo el metodo
+    ar.com.global.Utileria.imprimir("chau"); //asi se puede llamar un metodo de otro paquete sin importarlo previamente
+            
+    
+    }
+   
+}
